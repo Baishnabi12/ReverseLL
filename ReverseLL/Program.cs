a9463f70d46264839e3ee3b8166a96f57064826c
baish <baish@BAISHNABI>
2024-10-17 13:49:52 +0530

    Add project files.

---
+++ ReverseLL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        LinkedList<int> list = new LinkedList<int>();
+        list.AddLast(1);
+        list.AddLast(2);
+        list.AddLast(3);
+        var A = list.Reverse();
+        foreach (int x in A)
+        {
+            Console.WriteLine(x);
+        }
+    }
+
+}
